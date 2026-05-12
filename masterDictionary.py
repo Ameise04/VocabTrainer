14c1fa1b180e9ba1fd613a0e39e76ec3e826ad12
Jan Ameis <jameis@weinrich.de>
2026-05-12 16:21:02 +0200

    Added constructors for oop

---
+++ masterDictionary.py
@@ -0,0 +1,6 @@
+class MasterDictionary:
+    def __init__(self, language1, language2 = "deutsch"):
+        self.dictionaries = {}
+        self.language1 = language1
+        self.language2 = language2 
+        
